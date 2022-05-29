53373478e8bc0cec2a075881c87b67540932288d
jonathanmanoel <jonathan.silva.manoel@gmail.com>
2022-05-29 17:15:47 -0300

    Documentando funçõies com docstrings

---
+++ Funções/exemplo_5.py
@@ -0,0 +1,13 @@
+"""Documentação de Funções."""
+
+
+def soma(x,y):
+    #PEP - 257
+    """Soma 2 tipos somáveis.
+
+    Args:
+        - x Objeto somável
+        - y Objeto somável
+    :returns: soma de dois somáveis.
+    """
+    ...
